3c2f37562f17c00665f94857a9a6a6e54145b582
Olga Karavsheva <lelagorizont@yandex.ru>
2023-08-31 19:50:31 +0400

    add comments

--- main/pulsar/pulsar-admin.sh
+++ main/pulsar/pulsar-admin.sh
@@ -1,44 +1,45 @@
+#ledger представляет собой последовательное и неизменяемое хранилище для сообщений.
+
 # Create a tenant
-bin/pulsar-admin tenants create olga-tenant
+bin/pulsar-admin tenants create edna
 
 #посмотреть список тенантов
 bin/pulsar-admin tenants list
 
 # Create a namespace
-bin/pulsar-admin namespaces create olga-tenant/olga-namespace
+#bin/pulsar-admin namespaces create edna/olga-namespace
 
 #
-bin/pulsar-admin namespaces list olga-tenant
+bin/pulsar-admin namespaces list edna
 
 # Create a topic
-bin/pulsar-admin topics create olga-tenant/olga-namespace/olga-topic
+#bin/pulsar-admin topics create edna/olga-namespace/olga-topic
 
 #
-bin/pulsar-admin topics list olga-tenant/olga-namespace
+bin/pulsar-admin topics list edna/olga-namespace
 
 # без этих настроек топик быстро удаляется
-bin/pulsar-admin namespaces set-retention olga-tenant/olga-namespace --time -1  --size 1T
+#bin/pulsar-admin namespaces set-retention edna/olga-namespace --time -1  --size 1T
 
 
 #
-bin/pulsar-admin topics stats persistent://olga-tenant/olga-namespace/olga-topic
-bin/pulsar-admin topics stats olga-tenant/olga-namespace/olga-topic
+bin/pulsar-admin topics stats edna/transport/WhatsappLicenseBillEvent
 # "msgInCounter" : 3, — количество сообщений в топике
 
 # write message
-bin/pulsar-client produce persistent://olga-tenant/olga-namespace/olga-topic --messages 'Hello Olga!'
+#bin/pulsar-client produce persistent://edna/olga-namespace/olga-topic --messages 'Hello Olga!'
 
 #
-bin/pulsar-client consume persistent://olga-tenant/olga-namespace/olga-topic  -s 'my-subscription' -p Earliest -n 0
+#bin/pulsar-client consume persistent://edna/olga-namespace/olga-topic  -s 'my-subscription' -p Earliest -n 0
 
 # [Pulsar stats | Apache Pulsar](https://pulsar.apache.org/docs/3.1.x/administration-stats/#topic-internal-stats)
-bin/pulsar-admin topics stats-internal persistent://olga-tenant/olga-namespace/olga-topic
+bin/pulsar-admin topics stats-internal edna/transport/WhatsappLicenseBillEvent
 
 # [Manage topics | Apache Pulsar](https://pulsar.apache.org/docs/3.1.x/admin-api-topics/#get-subscriptions)
-bin/pulsar-admin topics subscriptions persistent://olga-tenant/olga-namespace/olga-topic
+bin/pulsar-admin topics subscriptions persistent://edna/olga-namespace/olga-topic
 
 #
-bin/pulsar compact-topic --topic persistent://olga-tenant/olga-namespace/olga-topic
+#bin/pulsar compact-topic --topic persistent://edna/olga-namespace/olga-topic
 
 #
 bin/pulsar-admin broker-stats --help
